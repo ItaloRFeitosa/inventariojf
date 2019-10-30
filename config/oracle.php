509b83a2af324c46be8eaed8b0e65318cb8c3fcf
Italo Ramos Feitosa <ma376vo@ma.trf1.gov.br>
2019-10-30 10:15:52 -0300

    oracle

--- config/oracle.php
+++ config/oracle.php
@@ -5,10 +5,10 @@ return [
         'driver' => 'oracle',
         'host' => '172.23.3.3',
         'port' => '1521',
-        'database' => '',
+        'database' => 'sicam',
         'service_name' => 'jfma.trf1.gov.br',
-        'username' => '',
-        'password' => '',
+        'username' => 'ma376vo',
+        'password' => 'jf2019italo',
         'charset' => '',
         'prefix' => '',
     ]
