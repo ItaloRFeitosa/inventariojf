51c25e667c586d2dd7ccbb4e96929d34612adb50
Andrey-Cadilhe <dinhocadilhe@gmail.com>
2019-11-08 11:44:00 -0300

    correcao de conflito blade

--- resources/views/admin/inventarios/includes/inventariosAtivos.blade.php
+++ resources/views/admin/inventarios/includes/inventariosAtivos.blade.php
@@ -1,11 +1,7 @@
 <div class="box box-info">
     <div class="box-header with-border">
         <h3 class="box-title" style="display: inline-block">Inventários Ativos</h3>
-<<<<<<< HEAD:resources/views/admin/inventario/includes/inventariosAtivos.blade.php
-        <a href="{{route('inventario.create')}}" class="btn btn-success pull-right"> Criar Novo Inventário</a>
-=======
         <a href="{{route('inventarios.create')}}" class="btn btn-success pull-right"> Criar Novo Inventário</a>
->>>>>>> 72803683f63c4f5c682cb7f9082497470ebd882d:resources/views/admin/inventarios/includes/inventariosAtivos.blade.php
     </div>
     <div class="box-body">
         <div class="row">
